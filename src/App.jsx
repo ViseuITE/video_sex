14b0d3b600f496b2bd5d09d9c3ca782d337bed80
momviseu software developer <momviseu@gmail.com>
2024-10-25 19:45:04 -0700

    1

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.css';
+
+import './charts/ChartjsConfig';
+
+// Import pages
+import Dashboard from './pages/Dashboard';
+import AddPost from './pages/post/AddPost';
+import DefaultLayout from './layouts/DefaultLayout';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+    <DefaultLayout>
+      <Routes>
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route path="/post" element={<AddPost/>} />
+      </Routes></DefaultLayout>
+    </>
+  );
+}
+
+export default App;
